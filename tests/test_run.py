aaee72ace292bf1318389403a00f156cc3ea7f0a
mukesh <mukeshp12z@gmail.com>
2021-11-19 22:20:10 +0530

    change requirements file, commented cursor.execute, changed test_run, changed config

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,7 +3,9 @@ from run import prepare_data, run_independent_sql
 import os
 import pathlib
 from config import logger
-
+import asyncio
+import pytest
+from config import cursor
 
 # test function to check if file contains tmp and prepare dictionary 
 def test_prepare_data_true():
@@ -22,16 +24,18 @@ def test_prepare_data_true():
     files = pathlib.Path('tmp')
     dict_dependency = {}
     
-    logger.info('dict1-',str(dict_dependency))
+    logger.info('dict1-'+str(dict_dependency))
     dict1 = prepare_data(files, dict_dependency)
     logger.info('dict2-'+str(dict1))
     
     assert dict1 == {'one.sql': ['tmp.two']}
 
+@pytest.mark.asyncio
 async def test_run_independent_sql():
     path = pathlib.Path('tmp')
     dependent_dict = {}
-    await run_independent_sql(path, dependent_dict)
+    sql = 'select * from tmp.dummy'
+    await run_independent_sql(cursor, sql, path)
 
     # here need to connect the table and check if the table created or any other check to validate
     # assert the validated value
